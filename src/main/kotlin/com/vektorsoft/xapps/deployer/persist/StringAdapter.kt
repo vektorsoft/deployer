b0732c963742dff7d8d770744e4fb0d5fa746d78
Vladimir Djurovic <vdjurovic@vektorsoft.com>
2018-12-31 11:24:31 +0100

    added adapter for empty strings

---
+++ src/main/kotlin/com/vektorsoft/xapps/deployer/persist/StringAdapter.kt
@@ -0,0 +1,36 @@
+/*
+ * Copyright (c) 2018. Vladimir Djurovic
+ *
+ * This file is part of Deployer.
+ *
+ * Deployer is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Deployer is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Deployer.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package com.vektorsoft.xapps.deployer.persist
+
+import javax.xml.bind.annotation.adapters.XmlAdapter
+
+
+class StringAdapter : XmlAdapter<String, String>() {
+
+    override fun marshal(value : String?): String?  {
+        println("marshalling: $value")
+        return if(value == "") null else value
+    }
+
+    override fun unmarshal(value: String?): String {
+        println("unmarshalling: $value")
+        return value ?: "";
+    }
+}
