a188124457ddcb8bed1e5a5bca7c11632454217c
Vladimir Djurovic <vdjurovic@vektorsoft.com>
2019-03-29 15:21:24 +0100

    added basic deployment functionality

--- src/main/kotlin/com/vektorsoft/xapps/deployer/ctrl/ProjectButtonsController.kt
+++ src/main/kotlin/com/vektorsoft/xapps/deployer/ctrl/ProjectButtonsController.kt
@@ -8,21 +8,39 @@
 
 package com.vektorsoft.xapps.deployer.ctrl
 
+import com.vektorsoft.xapps.deployer.client.DeployerClient
+import com.vektorsoft.xapps.deployer.client.DeployerException
+import com.vektorsoft.xapps.deployer.logger
 import com.vektorsoft.xapps.deployer.model.RuntimeData
 import com.vektorsoft.xapps.deployer.persist.XMLPersister
 import javafx.fxml.FXML
 import javafx.scene.control.Button
+import java.nio.file.Path
 
 class ProjectButtonsController {
 
-    @FXML
-    private lateinit var saveButton : Button
-
-    private lateinit var deployButton : Button
+    val LOGGER by  logger(ProjectButtonsController::class.java)
 
     @FXML
     fun saveProject() {
         val project = RuntimeData.selectedProjectItem.value.project ?: return
         XMLPersister.writeProject(project)
+    }
+
+    @FXML
+    fun deployProject() {
+        val project = RuntimeData.selectedProjectItem.value.project ?: return
+        val file = Path.of(project.location, "deployer-config.xml").toFile()
+        LOGGER.info("Deploying project {} from configuration file {}", project.name, file.absolutePath)
+
+        try {
+            val deployerClient = DeployerClient()
+            deployerClient.deploy(file, project.application.getServer().baseUrl)
+            LOGGER.info("Deployment successful!")
+        } catch(ex : DeployerException) {
+            LOGGER.error("Failed to deploy application", ex)
+        }
+
+
     }
 }
